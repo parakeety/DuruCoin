7764fe22595e0ba39cbd477013f97d4891b42ec2
yohei okada <okada.yohei@gmail.com>
2017-12-05 21:39:39 +0900

    Update initial migration file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const DuruCoin = artifacts.require("./DuruCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(DuruCoin);
 };
