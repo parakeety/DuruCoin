0b0cfb8f272ff46121441db973d6f532d702a9af
yohei okada <okada.yohei@gmail.com>
2017-12-06 21:37:23 +0900

    Setup truffle to deploy to ropsten

--- truffle.js
+++ truffle.js
@@ -1,4 +1,32 @@
-module.exports = {
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env.ROPSTEN_MNEMONIC;
+const accessToken = process.env.INFURA_ACCESS_TOKEN;
+
+// module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+  // ganache
+  // networks: {
+  //   development: {
+  //     host: 'localhost',
+  //     port: 7545,
+  //     network_id: 5777
+  //   }
+  // }
+// };
+
+module.exports = {
+  networks: {
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 4000000
+    }
+  }
 };
